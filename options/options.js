5f36fc0ae5365c6e7309bd88691c56f0e27d6f50
Crowdbotics <team@crowdbotics.com>
2021-08-30 16:11:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "pmz_test_30199Identifier",
+  url: "https://pmz_test_30199Identifier.botics.co",
+  api: "https://pmz_test_30199Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
